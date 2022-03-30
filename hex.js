6fc13bdba08c8459e2b6bf730de343c6fe61d6af
Lena Hou <lenahou2013@gmail.com>
2022-03-30 17:21:49 -0400

    getting a hex color

--- hex.js
+++ hex.js
@@ -1,3 +1,5 @@
+
+// we are going to set up a loop to generate a hex color on the fly
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
 // step-1: I am going to select the html element
@@ -7,5 +9,15 @@ const color = document.querySelector(".color")
 
 // step-2: add an event listener
 btn.addEventListener("click", function(){
-    
+    let hexColor = '#';
+    for (let i = 0; i < 6; i++) {
+        hexColor += hex[getRandomNumver()]
+    }
+
+    color.textContent = hexColor;
+    document.body.style.backgroundColor = hexColor;
 })
+
+function getRandomNumver(){
+    return Math.floor(Math.random() * hex.length)
+}
